3558d9160ba506cd43d08b5460ffea30a3a9c15e
Sergey Abu <dev@iteelion.ru>
2022-06-02 15:42:25 +0300

    Update bizproc.php

--- snippets/bizproc.php
+++ snippets/bizproc.php
@@ -1,4 +1,19 @@
 <?php
 
-// Start biz proc
-\CBPDocument::StartWorkflow(72, ['lists', 'BizprocDocument', $docID], [], $arErrorsTmp);
+// Start bizproc
+\CBPDocument::StartWorkflow($iblockId, ['lists', 'BizprocDocument', $docId], [], $arErrors);
+
+// Validate bizproc start
+\CBPDocument::StartWorkflowParametersValidate($templateId, $templateParameters, $documentType, $arErrors);
+
+// Stop bizproc
+\CBPDocument::killWorkflow($workflowId, $terminate, $documentId);
+
+// Get states
+\CBPDocument::GetDocumentStates($documentType, $documentId);
+
+// Get template
+\CBPDocument::getTemplatesForStart($userId, $documentType, $documentId, []);
+
+// Can user operate doc
+\CBPDocument::CanUserOperateDocument(\CBPCanUserOperateOperation::StartWorkflow, $userId, $documentId, []);
