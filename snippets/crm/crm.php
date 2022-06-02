2c750597c1e36630f49c83d8620197f4adacb4f5
Sergey Abu <dev@iteelion.ru>
2022-06-02 15:49:57 +0300

    add crm config libraries

--- snippets/crm/crm.php
+++ snippets/crm/crm.php
@@ -5,6 +5,9 @@ CCrmOwnerType::Undefined;
 CCrmActivityType::Undefined;
 $TT = new \Bitrix\Crm\Timeline\TimelineType();
 
+// Получение информации из справочников CRM
+$CrmStatusTable = new \Bitrix\Crm\StatusTable();
+
 // получить информацию по ИНН через bitrix (доступно только с оплаченной лицензией)
 if (\Bitrix\Main\Loader::includeModule('socialservices')) {
     $client = new \Bitrix\socialservices\properties\Client;
